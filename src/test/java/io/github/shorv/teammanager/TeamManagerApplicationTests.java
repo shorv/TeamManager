7d01384b4413b179d676eb016b904f4f2edbb209
shorv <kontakt.shorv@gmail.com>
2023-02-14 13:38:16 +0100

    Initial commit

---
+++ src/test/java/io/github/shorv/teammanager/TeamManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.shorv.teammanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeamManagerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
