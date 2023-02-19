a444462c4a290cb314c14ee990ce26d472f8e8f9
shorv <kontakt.shorv@gmail.com>
2023-02-19 14:13:58 +0100

    fix employee not found exception

--- src/main/java/io/github/shorv/teammanager/employee/EmployeeController.java
+++ src/main/java/io/github/shorv/teammanager/employee/EmployeeController.java
@@ -2,9 +2,6 @@ package io.github.shorv.teammanager.employee;
 
 import io.github.shorv.teammanager.employee.exception.EmployeeNotFoundException;
 import lombok.AllArgsConstructor;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ProblemDetail;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -15,7 +12,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
-import org.springframework.web.util.UriComponentsBuilder;
 
 import java.net.URI;
 import java.util.List;
@@ -32,15 +28,14 @@ public class EmployeeController {
                                                        @RequestParam(value = "size", required = false) Integer size,
                                                        @RequestParam(value = "sortDir", required = false) String sortDir,
                                                        @RequestParam(value = "sortField", required = false) String sortField) {
-        return ResponseEntity.ok()
-                .body(employeeService.getEmployees(page, size, sortDir, sortField));
+        return ResponseEntity.ok(employeeService.getEmployees(page, size, sortDir, sortField));
     }
 
     @GetMapping("{employeeId}")
     public ResponseEntity<Employee> getEmployeeById(@PathVariable("employeeId") Long employeeId) throws EmployeeNotFoundException {
         return employeeService.getEmployeeById(employeeId)
                 .map(ResponseEntity::ok)
-                .orElseThrow(() -> new EmployeeNotFoundException(String.format("Employee with id %d not found.", employeeId)));
+                .orElseThrow(EmployeeNotFoundException::new);
     }
 
     @PostMapping
--- src/main/java/io/github/shorv/teammanager/employee/exception/EmployeeNotFoundException.java
+++ src/main/java/io/github/shorv/teammanager/employee/exception/EmployeeNotFoundException.java
@@ -5,7 +5,4 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ResponseStatus(value = HttpStatus.NOT_FOUND, reason = "Employee not found")
 public class EmployeeNotFoundException extends Exception {
-    public EmployeeNotFoundException(String message) {
-        super(message);
-    }
 }
